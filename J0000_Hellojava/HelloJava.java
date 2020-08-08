c3bdf9f45d1fce1fc9aed7b99ea396f1979c625d
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-08 22:09:43 +0900

    Add files via upload

---
+++ J0000_Hellojava/HelloJava.java
@@ -0,0 +1,11 @@
+// HelloJava.java
+// https://github.com/waverlife/Java/tree/master/Java_source/J0000_Hellojava
+// 文字列[HelloJava]を標準出力する
+// メインクラスの宣言
+class HelloJava {
+  //メインメソッドの宣言 
+  public static void main(String[] args)  {
+  	//文の記述
+	System.out.println ( "Hello JAVA!!" ) ;
+  }
+}
